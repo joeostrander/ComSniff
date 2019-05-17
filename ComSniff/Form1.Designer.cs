e2f8b7a12e383b65edc7e1dda909be9aab2dbbb0
Joe Ostrander <ostrander.jm@gmail.com>
2019-05-17 14:08:51 -0500

    Add project files.

---
+++ ComSniff/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ComSniff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxComPort1 = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.comboBoxComPort2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSniffed = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxComPort1
+            // 
+            this.comboBoxComPort1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxComPort1.FormattingEnabled = true;
+            this.comboBoxComPort1.Location = new System.Drawing.Point(12, 25);
+            this.comboBoxComPort1.Name = "comboBoxComPort1";
+            this.comboBoxComPort1.Size = new System.Drawing.Size(407, 21);
+            this.comboBoxComPort1.TabIndex = 0;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(451, 85);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "&Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 305);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(551, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // comboBoxComPort2
+            // 
+            this.comboBoxComPort2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxComPort2.FormattingEnabled = true;
+            this.comboBoxComPort2.Location = new System.Drawing.Point(12, 87);
+            this.comboBoxComPort2.Name = "comboBoxComPort2";
+            this.comboBoxComPort2.Size = new System.Drawing.Size(407, 21);
+            this.comboBoxComPort2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "COM A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "COM B:";
+            // 
+            // textBoxSniffed
+            // 
+            this.textBoxSniffed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSniffed.Location = new System.Drawing.Point(12, 127);
+            this.textBoxSniffed.Multiline = true;
+            this.textBoxSniffed.Name = "textBoxSniffed";
+            this.textBoxSniffed.Size = new System.Drawing.Size(527, 175);
+            this.textBoxSniffed.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 327);
+            this.Controls.Add(this.textBoxSniffed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxComPort2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxComPort1);
+            this.MinimumSize = new System.Drawing.Size(567, 366);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxComPort1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox comboBoxComPort2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSniffed;
+    }
+}
+
---
+++ ComSniff/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace ComSniff
+{
+    public partial class Form1 : Form
+    {
+
+        private static List<KeyValuePair<string, string>> listPorts1 = new List<KeyValuePair<string, string>>();
+        private static List<KeyValuePair<string, string>> listPorts2 = new List<KeyValuePair<string, string>>();
+        private static bool boolConnected;
+
+        private static SerialPort serialPort1 = new SerialPort();
+        private static SerialPort serialPort2 = new SerialPort();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ThreadSafeDelegate(MethodInvoker method)
+        {
+            if (InvokeRequired)
+                BeginInvoke(method);
+            else
+                method.Invoke();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (boolConnected)
+            {
+                disconnect();
+            }
+            else
+            {
+                connect();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadPortsList();
+
+            comboBoxComPort1.DataSource = listPorts1;
+            comboBoxComPort1.ValueMember = "Key";
+            comboBoxComPort1.DisplayMember = "Value";
+
+            comboBoxComPort2.DataSource = listPorts2;
+            comboBoxComPort2.ValueMember = "Key";
+            comboBoxComPort2.DisplayMember = "Value";
+
+            this.Text = Application.ProductName;
+
+            serialPort1.BaudRate = 57600;
+            serialPort1.Parity = Parity.None;
+            serialPort1.Handshake = Handshake.RequestToSend; // ??
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+
+            serialPort2.BaudRate = 57600;
+            serialPort2.Parity = Parity.None;
+            serialPort2.Handshake = Handshake.RequestToSend; // ??
+            serialPort2.DataBits = 8;
+            serialPort2.StopBits = StopBits.One;
+
+            timer1.Enabled = true;
+        }
+
+
+        private void LoadPortsList()
+        {
+            listPorts1.Clear();
+            listPorts2.Clear();
+            string strQuery = "Select * from Win32_PnPEntity Where Name LIKE '% (COM%)'";
+
+            listPorts1.Add(new KeyValuePair<string, string>("", ""));
+            listPorts2.Add(new KeyValuePair<string, string>("", ""));
+
+            try
+            {
+
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(strQuery);
+                ManagementObjectCollection collection = searcher.Get();
+
+                foreach (ManagementObject item in collection)
+                {
+                    string fn = item.Properties["Name"].Value.ToString();
+                    string strPattern = @"^(?<porttext>.*?)\((?<portname>COM[\d]+)\)$";
+                    Match m = Regex.Match(fn, strPattern);
+
+                    if (m.Success)
+                    {
+                        string portname = m.Groups["portname"].Value.ToString();
+                        string porttext = m.Groups["porttext"].Value.ToString();
+                        listPorts1.Add(new KeyValuePair<string, string>(portname, portname.PadRight(6, ' ') + porttext));
+                        listPorts2.Add(new KeyValuePair<string, string>(portname, portname.PadRight(6, ' ') + porttext));
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not get serial port list!", "Serial Ports", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+        }
+
+
+        private void disconnect()
+        {
+            boolConnected = false;
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+
+            if (serialPort2.IsOpen)
+            {
+                serialPort2.Close();
+            }
+
+
+        }
+
+        private void connect()
+        {
+            string strPortName1 = comboBoxComPort1.SelectedValue.ToString();
+            string strPortName2 = comboBoxComPort2.SelectedValue.ToString();
+            if (string.IsNullOrEmpty(strPortName1) || string.IsNullOrEmpty(strPortName2))
+            {
+                return;
+            }
+            //serialPort1 = new SerialPort(strPortName, 9600, Parity.None, 8, StopBits.One);
+            serialPort1.PortName = strPortName1;
+            serialPort2.PortName = strPortName2;
+
+            try
+            {
+                serialPort1.Open();
+                serialPort2.Open();
+                boolConnected = true;
+
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+                serialPort2.DataReceived += new SerialDataReceivedEventHandler(serialPort2_DataReceived);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                boolConnected = false;
+
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+
+                if (serialPort2.IsOpen)
+                {
+                    serialPort2.Close();
+                }
+            }
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+            try
+            {
+                if (!string.IsNullOrEmpty(serialPort1.PortName) && !string.IsNullOrEmpty(serialPort2.PortName))
+                {
+                    if (serialPort1.IsOpen && serialPort2.IsOpen)
+                    {
+                        boolConnected = true;
+                        comboBoxComPort1.Enabled = false;
+                        comboBoxComPort2.Enabled = false;
+                    }
+                    else
+                    {
+                        boolConnected = false;
+                        comboBoxComPort1.Enabled = true;
+                        comboBoxComPort2.Enabled = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                boolConnected = false;
+                comboBoxComPort1.Enabled = true;
+                comboBoxComPort2.Enabled = true;
+            }
+
+
+
+
+            if (boolConnected)
+            {
+                buttonConnect.Text = "&Disconnect";
+                toolStripStatusLabel1.Text = "Connected to ports:  " + serialPort1.PortName + "," + serialPort2.PortName;
+            
+            }
+            else
+            {
+                buttonConnect.Text = "&Connect";
+                toolStripStatusLabel1.Text = "Disconnected";
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            boolConnected = false;
+
+        }
+
+
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            try
+            {
+
+                SerialPort sp = (SerialPort)sender;
+                int byteLength = sp.BytesToRead;
+                byte[] buffer = new byte[byteLength];
+                sp.Read(buffer, 0, byteLength);
+
+                string recvHex = BitConverter.ToString(buffer);
+                Console.WriteLine("{0}\tCOM A SENT:  {1}", DateTime.Now, recvHex + " ");
+
+
+                ThreadSafeDelegate(delegate { textBoxSniffed.AppendText(String.Format("A => B [ {0} ]", BitConverter.ToString(buffer)) + Environment.NewLine); });
+
+
+                //redirect to COM B
+                serialPort2.Write(buffer, 0, byteLength);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+        }
+
+        private void serialPort2_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+
+                SerialPort sp = (SerialPort)sender;
+                int byteLength = sp.BytesToRead;
+                byte[] buffer = new byte[byteLength];
+
+                sp.Read(buffer, 0, byteLength);
+
+                string recvHex = BitConverter.ToString(buffer);
+                Console.WriteLine("{0}\tCOM B SENT:  {1}", DateTime.Now, recvHex + " ");
+
+                ThreadSafeDelegate(delegate { textBoxSniffed.AppendText(String.Format("B => A [ {0} ]", BitConverter.ToString(buffer)) + Environment.NewLine); });
+
+                //redirect to port 1
+                serialPort1.Write(buffer, 0, byteLength);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+     
+    }
+}
